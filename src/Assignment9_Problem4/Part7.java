2acf9176f08e9e55a79baf94ad79e945cbe43749
eunbii0213 <eunbii0213@naver.com>
2021-11-30 20:41:34 +0900

    피드백 후 소스코드 수정

--- src/Assignment9_Problem4/Part7.java
+++ src/Assignment9_Problem4/Part7.java
@@ -2,84 +2,23 @@ package Assignment9_Problem4;
 
 public class Part7 {
 	
-	public int teddy(int initial, int goal, int increment, int count) {
-		
-		count++;
-		
-		int result=10;
-		
-		if(count>=10) {
-			return -1;
-		} else if(initial == goal) {
-			return count;
-		} else {
-			
-			int case1 = teddy(initial+=(initial/2),goal,increment,count);
-			int case2 = teddy(initial+=increment,goal,increment,count);
-			int case3 = teddy(initial-=(initial/2),goal,increment,count);
-			int case4 = teddy(initial-=increment,goal,increment,count);
-			
-			count = Math.min(case1, case2);
-			count = Math.min(result, case3);
-			count = Math.min(result, case4);
-			
-//			if(case1>=0) {
-//				if(case2>=0) {
-//					count = Math.min(case1, case2);
-//					
-//				}else {
-//					count = case1;
-//					
-//				}
-//			}
-//			
-//			if(case2>=0) {
-//				if(case1>=0) {
-//					count = Math.min(case1, case2);
-//					
-//				}else {
-//					count = case2;
-//					
-//				}
-//			}
-//			
-//			if(case3>=0) {
-//				if(case4>=0) {
-//					count = Math.min(result, case3);
-//					count = Math.min(result, case4);
-//					
-//				}else {
-//					count = Math.min(result, case3);
-//					
-//				}
-//			}
-//			
-//			if(case4>=0) {
-//				if(case3>=0) {
-//					count = Math.min(result, case3);
-//					count = Math.min(result, case4);
-//					
-//				}else {
-//					count = Math.min(result, case4);
-//					
-//				}
-//			}
-			
-			//System.out.println(result);
-		
-			return count;
-		}
-		
-		
-		
+	 private int teddy(int initial, int goal, int increment, int count) {
+	        if (count > 10) {
+	            return Integer.MAX_VALUE;
+	        } else if (initial == goal) {
+	            return count;
+	        }
+
+	        int case1 = initial % 2 == 0 ? teddy(initial + initial / 2, goal, increment, count + 1) : Integer.MAX_VALUE;
+	        int case2 = teddy(initial + increment, goal, increment, count + 1);
+	        int case3 = initial % 2 == 0 ? teddy(initial - initial / 2, goal, increment, count + 1) : Integer.MAX_VALUE;
+	        int case4 = teddy(initial - increment, goal, increment, count + 1);
+
+	        return Math.min(case1, Math.min(case2, Math.min(case3, case4)));
+	    }
+
+	    public int teddy(int initial, int goal, int increment) {
+	        int count = teddy(initial, goal, increment, 0);
+	        return count > 10 ? -1 : count;
+	    }
 	}
-
-	public int teddy(int initial, int goal, int increment) {
-		
-
-		return teddy(initial,goal,increment,-1);
-		
-		
-		
-	 }
-}
